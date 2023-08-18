796fa4ec606281bc40ec3026e2350bf804be4e5f
Iamneo <techadmin@iamneo.ai>
2023-08-18 06:55:41 +0000

    Backup Commit

--- springapp/src/main/java/com/demo/src/main/java/com/sasi/demo/DemoApplication.java
+++ springapp/src/main/java/com/demo/src/main/java/com/sasi/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@getMapping("Welcome")
 public class DemoApplication {
 
 	public string Welcome()
